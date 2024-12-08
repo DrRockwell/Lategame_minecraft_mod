7a86b5b51dafc7234e5d0e9b74f8cb34fb6ae79f
Brendan Lundell <54118419+DrRockwell@users.noreply.github.com>
2024-12-07 23:58:17 -0800

    add item .2

--- src/main/java/com/togl/rots/RemnnantsOfTheStars.java
+++ src/main/java/com/togl/rots/RemnnantsOfTheStars.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.togl.rots.init.item;
+
 public class RemnnantsOfTheStars implements ModInitializer {
 	public static final String MOD_ID = "remnnants-of-the-stars";
 
@@ -15,11 +17,8 @@ public class RemnnantsOfTheStars implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Loading...");
+		ItemInit.load();
 	}
 
 	public static Identifier id(String path) {
