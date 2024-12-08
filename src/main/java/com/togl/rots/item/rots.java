c0feb758d6d09bdc6ad79ba5e17ea863bd82fcc9
Brendan Lundell <54118419+DrRockwell@users.noreply.github.com>
2024-12-07 23:14:54 -0800

    Create rots.java

---
+++ src/main/java/com/togl/rots/item/rots.java
@@ -0,0 +1,11 @@
+public class rots {
+  
+	public static final Item SUSPICIOUS_SUBSTANCE = register(
+		new Item(new Item.Settings()),
+		"test_item"
+  );
+  
+  public static void initialize() {
+  }
+  
+}
