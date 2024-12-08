edc3347ab44a6cb2482fadb1b68344556fa1f50a
Brendan Lundell <54118419+DrRockwell@users.noreply.github.com>
2024-12-07 22:06:24 -0800

    Add files via upload

---
+++ src/client/java/com/lundell/rots/RemnnantsOfTheStarsClient.java
@@ -0,0 +1,10 @@
+package com.lundell.rots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RemnnantsOfTheStarsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
