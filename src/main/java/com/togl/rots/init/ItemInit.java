59cdc57538d79570f358e8658f2d73bab596906e
DrRockwell <brendan.lundell1@gmail.com>
2024-12-08 02:40:04 -0800

    Added Meteorite ore and Netherite fabric

--- src/main/java/com/togl/rots/init/ItemInit.java
+++ src/main/java/com/togl/rots/init/ItemInit.java
@@ -10,6 +10,11 @@ import net.minecraft.registry.Registry;
 public class ItemInit {
     public static final Item EXAMPLE_ITEM = register("example_item", new Item(new Item.Settings()));
 
+    public static final Item METEORITE_ORE = register("meteorite_ore",
+            new Item(new Item.Settings().fireproof()));
+
+    public static final Item NETHERITE_FABRIC = register("netherite_fabric",
+            new Item(new Item.Settings().fireproof()));
     
 
     public static <T extends Item> T register(String name, T item) {
