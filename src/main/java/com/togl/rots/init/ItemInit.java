6f1b99545f00813ab118ba0fc042ddf3a9b24c5a
DrRockwell <brendan.lundell1@gmail.com>
2024-12-08 13:38:27 -0800

    Added crafting recipe for netherite fabric

--- src/main/java/com/togl/rots/init/ItemInit.java
+++ src/main/java/com/togl/rots/init/ItemInit.java
@@ -1,6 +1,8 @@
 package com.togl.rots.init;
 
 import com.togl.rots.RemnantsOfTheStars;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -15,7 +17,12 @@ public class ItemInit {
 
     public static final Item NETHERITE_FABRIC = register("netherite_fabric",
             new Item(new Item.Settings().fireproof()));
-    
+    //
+      //       @Override
+     //       public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+    //            tooltip.add(Text.translatable("Test_Lore").formatted(Formatting.GOLD));
+    //        }
+    // I think the above way is how to add lore but I cant figure out if it even goes here
 
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, RemnantsOfTheStars.id(name), item);
