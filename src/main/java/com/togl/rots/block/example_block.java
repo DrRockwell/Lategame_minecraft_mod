0efe8135a88e0218b520036c82f9275d5fb818bf
DrRockwell <brendan.lundell1@gmail.com>
2024-12-13 01:56:46 -0800

    Restructure

--- src/main/java/com/togl/rots/block/example_block.java
+++
@@ -1,9 +0,0 @@
-package com.togl.rots.block;
-
-import net.minecraft.block.Block;
-
-public class example_block extends Block {
-    public example_block(Block.Settings settings) {
-        super(settings);
-    }
-}
